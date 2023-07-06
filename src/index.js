c8b604255a8f79ffde4884758c24f2833c4a5668
TshedisoB <tshedisoboshiana@gmail.com>
2023-07-06 17:57:52 +0200

    open image I want

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-import React from "react";
-import { render } from "react-dom";
-import Carousel from "./Carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState } from "react";
+import "./style.css";
 
-const App = () => (
-  <div>
-    <Carousel />
-  </div>
-);
+const ProjectViewer = () => {
+  const [popupImageVisible, setPopupImageVisible] = useState(false);
+  const [popupImageSrc, setPopupImageSrc] = useState("");
 
-render(<App />, document.getElementById("root"));
+  const handleImageClick = (src) => {
+    setPopupImageVisible(true);
+    setPopupImageSrc(src);
+  };
+
+  const handleCloseClick = () => {
+    setPopupImageVisible(false);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="header">Project Viewer</h1>
+
+      <div className="image-container">
+        <div className="image">
+          <img
+            src="./src/images/image1.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image1.png")}
+          />
+        </div>
+        <div className="image">
+          <img
+            src="./src/images/image2.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image2.png")}
+          />
+        </div>
+        <div className="image">
+          <img
+            src="./src/images/image3.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image3.png")}
+          />
+        </div>
+        <div className="image">
+          <img
+            src="./src/images/image4.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image4.png")}
+          />
+        </div>
+        <div className="image">
+          <img
+            src="./src/images/image5.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image5.png")}
+          />
+        </div>
+        <div className="image">
+          <img
+            src="./src/images/image6.png"
+            alt=""
+            onClick={() => handleImageClick("./src/images/image6.png")}
+          />
+        </div>
+      </div>
+
+      {popupImageVisible && (
+        <div className="popup-image">
+          <span onClick={handleCloseClick}>&times;</span>
+          <img src={popupImageSrc} alt="" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProjectViewer;
